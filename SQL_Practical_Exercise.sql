a46ba29e8cdc1c052d9dc722aff5b8c50f5925f6
mpwesthuizen <mp.westhuizen@gmail.com>
2020-05-16 18:10:06 +0100

    Completed SQL Practical

--- SQL_Practical_Exercise.sql
+++ SQL_Practical_Exercise.sql
@@ -30,8 +30,14 @@ SELECT TitleOfCourtesy+' '+FirstName+' '+LastName AS "Employee Name", City
 FROM Employees
 WHERE Country LIKE('UK')
 
--- Q1.6 INCOMPLETE
-SELECT sum the units with the discount 
+-- Q1.6 
+SELECT Region.RegionID, FORMAT(SUM((UnitPrice *(1- Discount))* Quantity),'C') "Total Discount Price"   
+FROM [Order Details]
+INNER JOIN Orders ON Orders.OrderID = [Order Details].OrderID
+INNER JOIN EmployeeTerritories ON Orders.EmployeeID = EmployeeTerritories.EmployeeID
+INNER JOIN Territories ON EmployeeTerritories.TerritoryID = Territories.TerritoryID
+INNER JOIN Region ON Territories.RegionID = Region.RegionID
+GROUP BY  Region.RegionID HAVING SUM((UnitPrice *(1- Discount))* Quantity) > 1000000
 
 -- Q1.7 
 SELECT COUNT(*) AS "Total UK and USA Freight with over 100 orders"
@@ -40,11 +46,11 @@ WHERE Freight > 100 AND ShipCountry IN ('UK', 'USA')
 
 
 -- Q1.8 
-SELECT TOP 1 OrderID, (UnitPrice * Quantity) * (1*Discount) AS "Discounted Totals"
+SELECT TOP 1 OrderID, UnitPrice * (1*Discount) * Quantity  AS "Discounted Totals"
 FROM [Order Details] 
 ORDER BY "Discounted Totals" DESC
 
----------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Q2.1
 CREATE TABLE Spartans
 (
@@ -107,13 +113,31 @@ VALUES
 SELECT * FROM Spartans
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- Q3.1 INCOMPLETE
+-- Q3.1 
 SELECT TitleOfCourtesy+' '+FirstName+' '+LastName AS "Employee Name", ReportsTo
 FROM Employees
 
-SELECT * FROM Employees
--- Q3.2 INCOMPLETE
+-- Q3.2(EXCEL) 
 -- USing SQL with EXCEL. First Create a new folder. Then export the data via 'task' and export, select Excel. Complete the exprot wizard. When fininshed the data should be in an excel file within the file specified.
+SELECT CompanyName, FORMAT(SUM([Order Details].UnitPrice * Quantity),'C') AS "Supplier Total"
+FROM [Order Details]
+INNER JOIN Products ON [Order Details].ProductID = Products.ProductID
+INNER JOIN  Suppliers ON Products.SupplierID = Suppliers.SupplierID 
+GROUP BY  CompanyName HAVING SUM([Order Details].UnitPrice * Quantity)> 10000 
+ORDER BY SUM([Order Details].UnitPrice * Quantity) DESC  
 
--- Q3.3 INCOMPLETE
--- Q3.4 INCOMPLETE
+-- Q3.3
+SELECT TOP 10 CompanyName, ContactName, FORMAT(SUM(([Order Details].UnitPrice * (1-Discount)) * Quantity),'C') AS "Shippped Orders Total"
+FROM Customers
+INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+INNER JOIN [Order Details] ON  Orders.OrderID = [Order Details].OrderID
+WHERE ShippedDate BETWEEN '1998-01-01' AND '1998-05-06'
+GROUP BY CompanyName, ContactName ORDER BY SUM(([Order Details].UnitPrice *(1-Discount))* Quantity) DESC
+ 
+-- Q3.4(EXCEL)
+SELECT AVG(DATEDIFF(dd, OrderDate, ShippedDate)) AS "Average Amount of Shipping Days ", FORMAT(ShippedDate, 'MMM-yy') AS "Shipping Month"
+FROM Orders
+WHERE ShippedDate IS NOT NULL
+GROUP BY YEAR(ShippedDate), MONTH(ShippedDate), FORMAT(ShippedDate, 'MMM-yy') ORDER BY YEAR(ShippedDate), MONTH(ShippedDate)
+
+ 
